f6b394bf34da463f4c47728d91bacd9541d9157f
Lars Soltau <ls@profics.ch>
2016-03-11 18:06:17 +0100

    changed secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = 'dcce07a35738a891bd855148eee67d46d90276751fc1c456f39e3da1c09dd47ed3bfaa450c9e9699b3b3a333230bcd01ab73685e69f4b4651673be392e45983e'
+Depot::Application.config.secret_token = '3cce07a35738a891bd855148eee67d46d90276751fc1c456f39e3da1c09dd47ed3bfaa450c9e9699b3b3a333230bcd01ab73685e69f4b4651673be392e45983e'
